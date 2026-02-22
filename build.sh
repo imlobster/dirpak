ad94e1b87d2964929cb0f102754857037e580533
imlobster <zhizhilik@gmail.com>
2026-02-22 12:38:24 +0200

    removed cmake

--- build.sh
+++ build.sh
@@ -1,8 +1,22 @@
-#!/bin/sh
+#!/usr/bin/env sh
 
-mkdir -p build
-cd build || exit 1
-cmake ..
-printf "\nbuilding...\n\n"
-cmake --build $@ .
-cd ..
+for c in c++ clang++ g++; do
+	command -v "$c" >/dev/null 2>&1 && { compiler="$c"; break; }
+done
+
+if [ -z "$compiler" ]; then
+	printf "%s: no matching c++ compiler found!\n" "$0" >&2
+	exit 1
+fi
+
+target="dirpak.cc"
+output="dirpak"
+
+PS4=''
+{ set -x; } 2>/dev/null
+
+"$compiler" "$target" "-o" "$output" "-O3" "-Wall" "-Wextra" "-Werror=return-type" "-fno-rtti" "-fno-exceptions"
+
+{ set +x; } 2>/dev/null
+
+exit
